f3718fa9f0192d7923a65276d1012a8c0d6126c3
10.0.8.22 <salljaune>
2025-12-11 18:01:25 +0000

    add new camera model

--- baslerCam.py
+++ baslerCam.py
@@ -166,79 +166,92 @@ class BASLER (QtCore.QThread):
     def setCamParameter(self): 
         """Set initial parameters
         """
-               
+        self.camLanguage = dict()       
         self.cam0.Open()
         self.camID = self.cam0.GetDeviceInfo().GetSerialNumber()
         print(' connected@IP: ',self.cam0.GetDeviceInfo().GetIpAddress() )
-                
         
+        self.model = self.cam0.DeviceModelName.GetValue()
+        print('model',self.model)
+        if self.model == 'a2A4096-9gmBAS' or 'a2A3840-13gmBAS':
+            self.camLanguage['exposure'] = 'ExposureTime'
+            self.camLanguage['gain'] = 'Gain'
+        else :
+            self.camLanguage['exposure'] = 'ExposureTimeAbs'
+            self.camLanguage['gain'] = 'GainRaw'
+        print('model',self.model)
+        
+        self.attExp = getattr(self.cam0,self.camLanguage['exposure'])
+        self.attGain = getattr(self.cam0,self.camLanguage['gain'])
+        
+
         self.LineTrigger = str(self.conf.value(self.nbcam+"/LineTrigger")) # for 
         
         self.cam0.TriggerMode.SetValue('Off')
-        self.cam0.TriggerActivation.SetValue('RisingEdge')
+        
+        
+        #self.cam0.TriggerActivation.SetValue('RisingEdge')
+        
         self.cam0.TriggerSource.SetValue(self.LineTrigger )
         self.cam0.ExposureAuto.SetValue('Off')
+        
         self.cam0.GainAuto.SetValue('Off')
         
         self.cam0.Width = self.cam0.Width.Max  # set camera width at maximum
         self.cam0.Height = self.cam0.Height.Max
         
         
-        self.camParameter["expMax"] = float(self.cam0.ExposureTimeAbs.GetMax()/1000)
-        self.camParameter["expMin"] = float(self.cam0.ExposureTimeAbs.GetMin()/1000)
         
-        self.camParameter["gainMax"]=self.cam0.GainRaw.GetMax()
-        self.camParameter["gainMin"]=self.cam0.GainRaw.GetMin()
+        self.camParameter["expMax"] = float(self.attExp.GetMax()/1000)
+        self.camParameter["expMin"] = float(self.attExp.GetMin()/1000)  #Change minimum and mAximum value from getMin/1000 to Getmin
         
+        self.camParameter["gainMax"] = self.attGain.GetMax()
+        self.camParameter["gainMin"]= self.attGain.GetMin()
         #if exposure time save in the ini file is not in the range we put the minimum
+        
         if self.camParameter["expMin"] <=float(self.conf.value(self.nbcam+"/shutter"))<=self.camParameter["expMax"]:
-            self.cam0.ExposureTimeAbs.SetValue(float(self.conf.value(self.nbcam+"/shutter"))*1000)
+            self.attExp.SetValue(float(self.conf.value(self.nbcam+"/shutter"))*1000)
         else:
-            self.cam0.ExposureTimeAbs.SetValue(self.camParameter["expMin"]*1000)
-    
-        self.camParameter["exposureTime"]=int(self.cam0.ExposureTimeAbs.GetValue())/1000
+            self.attExp.SetValue(self.camParameter["expMin"]*1000)
+        
+        self.camParameter["exposureTime"]=int(self.attExp.GetValue())/1000
         
         
         if self.camParameter["gainMin"] <=int(self.conf.value(self.nbcam+"/gain"))<=self.camParameter["gainMax"]:
-            self.cam0.GainRaw.SetValue(int(self.conf.value(self.nbcam+"/gain")))
+            self.attGain.SetValue(int(self.conf.value(self.nbcam+"/gain")))
         else:
             print('gain error: gain set to minimum value')
-            self.cam0.GainRaw.SetValue(int(self.camParameter["gainMin"]))
+            self.attGain.SetValue(int(self.camParameter["gainMin"]))
         
-        self.camParameter["gain"]=self.cam0.GainRaw.GetValue()
+        self.camParameter["gain"] = self.attGain.GetValue()
+        
+        self.camParameter["trigger"] = self.cam0.TriggerMode.GetValue()
+        
+        self.threadRunAcq = ThreadRunAcq(self)
         
         
-        self.camParameter["trigger"]=self.cam0.TriggerMode.GetValue()
-        
-        self.threadRunAcq=ThreadRunAcq(self)
-        
-        
-        if self.multi==True:
+        if self.multi == True:
             self.threadRunAcq.newDataRun.connect(self.newImageReceived,QtCore.Qt.DirectConnection)
         else:
             self.threadRunAcq.newDataRun.connect(self.newImageReceived)
-           
-            
-        self.threadOneAcq=ThreadOneAcq(self)
+             
+        self.threadOneAcq = ThreadOneAcq(self)
         self.threadOneAcq.newDataRun.connect(self.newImageReceived)
         self.threadOneAcq.newStateCam.connect(self.stateCam)
-            
-            
+             
     def setExposure(self,sh):
         ''' set exposure time in ms
         '''
-        
-        self.cam0.ExposureTimeAbs.SetValue(float (sh*1000))# in balser ccd exposure time is microsecond
-        self.camParameter["exposureTime"]=float(self.cam0.ExposureTimeAbs.GetValue())/1000
-        print("exposure time is set to",float(self.cam0.ExposureTimeAbs.GetValue()),' micro s')
+        self.attExp.SetValue(float (sh*1000))# in balser ccd exposure time is microsecond  # Changed the value from sh*1000 to sh
+        self.camParameter["exposureTime"]=float(self.attExp.GetValue()/1000)
+        print("exposure time is set to",float(self.attExp.GetValue()),' micro s')
         
     def setGain(self,g):
         ''' set gain 
         '''
-        
-        self.cam0.GainRaw.SetValue(int(g)) # 
-        print("Gain is set to",self.cam0.GainRaw.GetValue())   
-        self.camParameter["gain"]=self.cam0.GainRaw.GetValue()
+        self.attGain.SetValue(int(g)) # 
+        print("Gain is set to",self.attGain.GetValue())   
+        self.camParameter["gain"]=self.attGain.GainRaw.GetValue()
     
     def softTrigger(self):
         '''to have a sofware trigger
@@ -249,56 +262,47 @@ class BASLER (QtCore.QThread):
     def setTrigger(self,trig='off'):
         '''set trigger mode on/off
         '''
-        
-        if trig=='on':
+        if trig == 'on':
             self.cam0.TriggerMode.SetValue('On')
-            self.itrig='on'
+            self.itrig = 'on'
         else:
             self.cam0.TriggerMode.SetValue('Off')
             
-            self.itrig='off'
-        
+            self.itrig = 'off'
         self.camParameter["trigger"]=self.cam0.TriggerMode.GetValue()
         
     def startAcq(self):
         '''Acquistion in live mode
         '''
-        
-        self.camIsRunning=True
+        self.camIsRunning = True
         self.threadRunAcq.newRun() # to set stopRunAcq=False
         self.threadRunAcq.start()
     
     def startOneAcq(self,nbShot):
         '''Acquisition of a number (nbShot) of image 
         '''
-        
-        self.nbShot=nbShot 
-        self.camIsRunning=True
+        self.nbShot = nbShot 
+        self.camIsRunning = True
         self.threadOneAcq.newRun() # to set stopRunAcq=False
         self.threadOneAcq.start()
         
     def stopAcq(self):
-        
         self.threadRunAcq.stopThreadRunAcq()
         # if self.threadRunAcq.isRunning():
         #     self.threadRunAcq.terminate()
         self.threadOneAcq.stopThreadOneAcq()
-        self.camIsRunnig=False  
+        self.camIsRunnig = False  
     
     def newImageReceived(self,data):
         '''Emit the data when receive a data from the thread threadRunAcq threadOneAcq
         '''
-        
-        self.data=data
-        
+        self.data = data
         self.newData.emit(self.data)
-    
         
     def stateCam(self,state):
         '''state of camera : True is running False : is stopped
         '''
-        
-        self.camIsRunning=state
+        self.camIsRunning = state
         
     def endAcquisition(self):
         self.endAcq.emit(True)
@@ -308,54 +312,50 @@ class BASLER (QtCore.QThread):
         self.cam0.Close()
         
         
-        
-        
 class ThreadRunAcq(QtCore.QThread):
     '''Second thread for controling continus acquisition independtly
     '''
-    newDataRun=QtCore.pyqtSignal(object)
+    newDataRun = QtCore.pyqtSignal(object)
     
     def __init__(self, parent):
         
         super(ThreadRunAcq,self).__init__(parent)
-        self.parent=parent
+        self.parent = parent
         self.cam0 = parent.cam0
-        self.stopRunAcq=False
-        self.itrig= parent.itrig
-        self.LineTrigger=parent.LineTrigger
-        self.converter=pylon.ImageFormatConverter()
+        self.stopRunAcq = False
+        self.itrig = parent.itrig
+        self.LineTrigger = parent.LineTrigger
+        self.converter = pylon.ImageFormatConverter()
         
     def newRun(self):
-        self.stopRunAcq=False
+        self.stopRunAcq = False
 
       
     def run(self):
         
         while self.stopRunAcq is not True :
-            # self.mutex.lock()
             try :
-                data=self.cam0.GrabOne(20000000)
+                data = self.cam0.GrabOne(20000000)
             except :
                 self.cam0.StopGrabbing()
-                data=self.cam0.GrabOne(20000000)
-            # data=self.converter.Convert(data)
+                data = self.cam0.GrabOne(20000000)
+            
             data = data.GetArray()#, dtype=np.double)
             data.squeeze()
-            
             self.data=np.rot90(data,1)
             
             if np.max(self.data)>0: # send data if not zero 
                 
-                if self.stopRunAcq==True:
+                if self.stopRunAcq is True:
                     break
                 else :
                     self.newDataRun.emit(self.data)
                     
                     # print(self.cam0.DeviceTemperature.GetValue())
-            # self.mutex.unlock()
+    
     def stopThreadRunAcq(self):
         
-        self.stopRunAcq=True
+        self.stopRunAcq = True
         
         try :
             self.cam0.ExecuteSoftwareTrigger()
@@ -366,39 +366,36 @@ class ThreadRunAcq(QtCore.QThread):
         self.cam0.Close()
         
         
-        
 class ThreadOneAcq(QtCore.QThread):
     '''Second thread for controling one or anumber of  acquisition independtly
     '''
-    newDataRun=QtCore.pyqtSignal(object)
-    newStateCam=QtCore.pyqtSignal(bool) # signal to emit the state (running or not) of the camera
+    newDataRun = QtCore.pyqtSignal(object)
+    newStateCam = QtCore.pyqtSignal(bool) # signal to emit the state (running or not) of the camera
     
     def __init__(self, parent):
         
         super(ThreadOneAcq,self).__init__()
-        self.parent=parent
+        self.parent = parent
         self.cam0 = parent.cam0
-        self.stopRunAcq=False
-        self.itrig= parent.itrig
-        self.LineTrigger=parent.LineTrigger
-        self.converter=pylon.ImageFormatConverter()
-   
-    
+        self.stopRunAcq = False
+        self.itrig = parent.itrig
+        self.LineTrigger = parent.LineTrigger
+        self.converter = pylon.ImageFormatConverter()
+
     def newRun(self):
-        self.stopRunAcq=False
+        self.stopRunAcq = False
         
-      
     def run(self):
         self.newStateCam.emit(True)
         
         for i in range (self.parent.nbShot):
             if self.stopRunAcq is not True :
-                data=self.cam0.GrabOne(200000)
-                data=self.converter.Convert(data)
+                data = self.cam0.GrabOne(200000)
+                data = self.converter.Convert(data)
                 data = data.GetArray()#, dtype=np.double)
                 data.squeeze()
             
-                self.data=np.rot90(data,1)
+                self.data = np.rot90(data,1)
                 
                 if i<self.parent.nbShot-1:
                     self.newStateCam.emit(True)
@@ -416,16 +413,13 @@ class ThreadOneAcq(QtCore.QThread):
        
     def stopThreadOneAcq(self):
         
-        self.stopRunAcq=True
+        self.stopRunAcq = True
         
         try :
             self.cam0.ExecuteSoftwareTrigger()
         except :
             pass      
         
-    
-        
-    
         
 if __name__ == "__main__":       
     
